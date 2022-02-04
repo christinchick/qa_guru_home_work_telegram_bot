61030fc9c2c0762a40517fe1ff72d04dc37cffd6
christinchick <alexey@skyward-crimea.ru>
2022-02-04 13:13:53 +0300

    new

---
+++ src/test/java/BotTests.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class BotTests {
+
+        @Test
+        void test00() {
+            assertTrue(true);
+        }
+
+        @Test
+        void test01() {
+            assertTrue(true);
+        }
+
+        @Test
+        void test02() {
+            assertTrue(true);
+        }
+}
---
+++ src/test/java/NegativBotTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class NegativBotTests {
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(false);
+    }
+}
